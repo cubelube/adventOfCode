a2fc6be07e35161ca6f0d1592e71a867d24d83d9
cubelube <114615759+cubelube@users.noreply.github.com>
2023-12-08 23:06:50 -0500

    Create part1.cpp

---
+++ 2015/Day 1/part1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+
+int main() {
+    std::string paren;
+    int counter = 0;
+    std::ifstream ReadFile("part1input.txt");
+
+    while(getline(ReadFile, paren)) {
+        for(int i = 0; i < paren.size(); i++) {
+            if(paren[i] == '(') {
+                counter += 1;
+            } else {
+                counter -= 1;
+            }
+        }
+    }
+
+    std::cout << counter << "\n";
+}
