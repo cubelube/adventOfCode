ad6ac54f242bd620ddb1afecc89634b1c24311f7
cubelube <114615759+cubelube@users.noreply.github.com>
2023-12-09 11:56:26 -0500

    Create d1p1.cpp

---
+++ 2015/d1p1.cpp
@@ -0,0 +1,24 @@
+// https://adventofcode.com/2023/day/1
+
+#include <iostream>
+#include <fstream>
+
+int main() {
+    std::string paren;
+    int counter = 0;
+    std::ifstream ReadFile("part1input.txt");
+
+    while(getline(ReadFile, paren)) {
+        for(int i = 0; i < paren.size(); i++) {
+            if(paren[i] == '(') {
+                counter += 1;
+            } else {
+                counter -= 1;
+            }
+        }
+    }
+
+    std::cout << counter << "\n";
+
+  return 0;
+}
