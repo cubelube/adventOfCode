d1a167421dc7f584d90daeacb2e1bdb16a91652f
cubelube <114615759+cubelube@users.noreply.github.com>
2023-12-20 15:57:05 -0500

    Create d1p2.rs

---
+++ 2015/Day 1/d1p2.rs
@@ -0,0 +1,22 @@
+use std::fs;
+
+fn main() {
+    let mut counter:i32 = 0;
+    let file_text = fs::read_to_string("input.txt")
+        .expect("Failed");
+
+    for (i, char) in file_text.chars().enumerate() {
+        if char == '(' {
+            counter += 1;
+        } else {
+            counter -= 1;
+        }
+
+        if counter < 0 {
+            println!("{i}");
+            break;
+        }
+    }
+    
+    println!("{counter}");
+}
